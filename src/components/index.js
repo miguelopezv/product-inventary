84b17241418290f89180682f62dd825db66ea696
Miguel López <miguellopez@vitalware.com>
2019-08-26 16:23:54 -0500

    barrel

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import AddProduct from './AddProduct';
+import EditProduct from './EditProduct';
+import Products from './Products';
+import Product from './Product';
+import Header from './Header';
+
+export { AddProduct, EditProduct, Products, Product, Header };
