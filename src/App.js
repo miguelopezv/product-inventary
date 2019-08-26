910346b3055b856ef4173dfbfb93244150a5e9fe
Miguel López <miguellopez@vitalware.com>
2019-08-26 16:23:34 -0500

    create routes to be used

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import {
+  Products,
+  AddProduct,
+  EditProduct,
+  Product,
+  Header
+} from './components';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <Header />
+      <main className="container mt-5">
+        <Switch>
+          <Route exact path="/add-product" component={AddProduct} />
+          <Route exact path="/products" component={Products} />
+          <Route exact path="/products/edit/:id" component={EditProduct} />
+          <Route exact path="/products/:id" component={Product} />
+        </Switch>
+      </main>
+      <p className="text-center mt-4 p2">All rights reserved.</p>
+    </Router>
+  );
 }
 
 export default App;
