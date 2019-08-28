c2db75e77a347b580c67fe12df13f4727c4aebbb
Miguel López <miguellopez@vitalware.com>
2019-08-28 10:47:23 -0500

    add state and effect to get products

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import axios from 'axios';
 import {
   Products,
   AddProduct,
@@ -9,13 +10,26 @@ import {
 } from './components';
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    getProducts().then(res => setProducts(res.data));
+  }, []);
+
+  const getProducts = async () =>
+    await axios.get(process.env.REACT_APP_API_URL);
+
   return (
     <Router>
       <Header />
       <main className="container mt-5">
         <Switch>
           <Route exact path="/add-product" component={AddProduct} />
-          <Route exact path="/products" component={Products} />
+          <Route
+            exact
+            path="/products"
+            render={() => <Products products={products}></Products>}
+          />
           <Route exact path="/products/edit/:id" component={EditProduct} />
           <Route exact path="/products/:id" component={Product} />
         </Switch>
