7a14db6dfe135f0995804052534d52e8b349e749
Miguel López <miguellopez@vitalware.com>
2019-08-30 12:53:08 -0500

    delete one product

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,8 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
+import Swal from 'sweetalert2';
 
-const Product = ({ product }) => {
-  const onClickHandler = id => {};
+const Product = ({ product, history, setRequest }) => {
+  const onClickHandler = id => {
+    Swal.fire({
+      title: 'Are you sure?',
+      text: "You won't be able to revert this!",
+      type: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!'
+    }).then(async result => {
+      if (result.value) {
+        try {
+          await axios.delete(`${process.env.REACT_APP_API_URL}/${product.id}`);
+          Swal.fire('Deleted!', 'Your file has been deleted.', 'success');
+          setRequest(true);
+        } catch (error) {
+          const err = error.toJSON();
+          Swal.fire('Error', err.message, 'error');
+        }
+      }
+    });
+  };
 
   return (
     <li
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,12 +1,16 @@
 import React, { Fragment } from 'react';
 import Product from './Product';
 
-const Products = ({ products }) => (
+const Products = ({ products, setRequest }) => (
   <Fragment>
     <h1 className="text-center">Products</h1>
     <ul className="list-group mt-5">
       {products.map(product => (
-        <Product key={product.id} product={product}></Product>
+        <Product
+          setRequest={setRequest}
+          key={product.id}
+          product={product}
+        ></Product>
       ))}
     </ul>
   </Fragment>
